cd967dc3e7580642efe2b49a476da1043ff044be
Mingjia Wang <mingjiawangdev@gmail.com>
2020-03-15 17:11:35 -0700

    added several ViewController files

---
+++ Budgetly/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  Budgetly
+//
+//  Created by Mingjia Wang on 3/15/20.
+//  Copyright © 2020 nguyenethan01. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
