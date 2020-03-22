76958850663a7ce04235add9aa9ff02dc8c71f18
nguyenethan01 <nguyenethan01@gmail.com>
2020-03-22 12:33:47 -0700

    Finalized the LoginSegueTwo, User can now login and signup

--- Budgetly/LoginViewController.swift
+++ Budgetly/LoginViewController.swift
@@ -21,8 +21,34 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func onSignIn(_ sender: Any) {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "loginSegueTwo", sender: nil)
+            } else {
+                print ("Error: \(error?.localizedDescription)")
+            }
+        }
+        
     }
+    
+    
     @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegueTwo", sender: nil)
+            } else{
+                print ("Error: \(error?.localizedDescription)")
+            }
+        }
+        
     }
     
 
