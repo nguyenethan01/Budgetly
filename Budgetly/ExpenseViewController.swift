8111ec74fe9727430c8edc378b7ffa25f4f18dea
nguyenethan01 <nguyenethan01@gmail.com>
2020-03-22 22:57:39 -0700

    Created PieChart for Breakdown

--- Budgetly/ExpenseViewController.swift
+++ Budgetly/ExpenseViewController.swift
@@ -7,16 +7,54 @@
 //
 
 import UIKit
+import Charts
+import Parse
 
 class ExpenseViewController: UIViewController {
 
     @IBOutlet weak var spendingImageView: UIImageView!
 
-
+    @IBOutlet weak var pieChart: PieChartView!
+    
+    var items = [PFObject]()
+    var curr = [PieChartDataEntry]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //find current date
+        let now = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "LLLL"
+        let nameOfMonth = dateFormatter.string(from: now)
+        //set up queries
+        let query = PFQuery(className: "Items")
+        query.whereKey("author",equalTo: PFUser.current())
+        query.whereKey("month",equalTo: nameOfMonth)
+        query.findObjectsInBackground { (items, error) in
+            if items != nil{
+                for i in items!{
+                    let temp = i["amount"] as! String
+                    let val = Double(temp)
+      
+                    let label = i["category"]
+                    self.curr.append(PieChartDataEntry(value: val ?? 0, label: (label as! String)))
+                }
+                let dataSet = PieChartDataSet(entries:self.curr, label: "categories")
+                let data = PieChartData(dataSet:dataSet)
+                self.pieChart.data = data
+                self.pieChart.chartDescription?.text = "Expenses"
+                dataSet.colors = ChartColorTemplates.joyful()
+                dataSet.valueColors = [UIColor.black]
+                self.pieChart.legend.font = UIFont(name: "Futura", size: 10)!
 
+            }
+        }
+        
+        
+        
+        
+        
         spendingImageView.layer.cornerRadius = 10
         spendingImageView.clipsToBounds = true
         // Do any additional setup after loading the view.
